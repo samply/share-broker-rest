8d4fde2bbbb629f3762a917724d993aa25a02e85
tasd <d.tas@dkfz-heidelberg.de>
2018-09-26 09:01:08 +0200

    changed sort algorithm

--- src/main/java/de/samply/share/broker/utils/db/ReplyUtil.java
+++ src/main/java/de/samply/share/broker/utils/db/ReplyUtil.java
@@ -24,7 +24,7 @@ public class ReplyUtil {
             Configuration configuration = new DefaultConfiguration().set(conn).set(SQLDialect.POSTGRES);
             ReplyDao replyDao = new ReplyDao(configuration);
             List<Reply> reply = replyDao.fetchByInquiryId(inquiryID);
-            reply = sortList(reply);
+            Collections.sort(reply, new SampleOperator());
             return reply;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -32,27 +32,22 @@ public class ReplyUtil {
         return null;
     }
 
-    private static List<Reply> sortList(List<Reply> replyList) {
-        JSONParser parser = new JSONParser();
-        try {
-            for (int i = 0; i < replyList.size(); i++) {
-                for (int j = 1; j < replyList.size()-i; j++) {
-                    Reply reply = replyList.get(j-1);
-                    JSONObject jsonObject = (JSONObject) parser.parse(reply.getContent());
-                    int size = Integer.parseInt(jsonObject.get("sample").toString());
-                    Reply reply2 = replyList.get(j);
-                    JSONObject jsonObject2 = (JSONObject) parser.parse(reply2.getContent());
-                    int size2 = Integer.parseInt(jsonObject2.get("sample").toString());
-                    if (size > size2) {
-                        replyList.set(j, reply);
-                        replyList.set(j-1, reply2);
-                    }
-                }
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return replyList;
-    }
+    static class SampleOperator implements Comparator<Reply> {
 
+        @Override
+        public int compare(Reply reply1, Reply reply2) {
+            int size = 0;
+            int size2 = 0;
+            try {
+                JSONParser parser = new JSONParser();
+                JSONObject jsonObject = (JSONObject) parser.parse(reply1.getContent());
+                size = Integer.parseInt(jsonObject.get("sample").toString());
+                JSONObject jsonObject2 = (JSONObject) parser.parse(reply2.getContent());
+                size2 = Integer.parseInt(jsonObject2.get("sample").toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return size < size2 ? 1 : size == size2 ? 0 : -1;
+        }
+    }
 }
