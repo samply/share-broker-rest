6f59dd6026d3bac703dae4428189a4d1a51d0708
Alexander Kiel <alexanderkiel@gmx.net>
2020-04-28 16:35:05 +0200

    Add Stratifier to CQL Expressions

--- src/main/java/de/samply/share/broker/utils/cql/EssentialSimpleQueryDto2CqlTransformer.java
+++ src/main/java/de/samply/share/broker/utils/cql/EssentialSimpleQueryDto2CqlTransformer.java
@@ -2,7 +2,12 @@ package de.samply.share.broker.utils.cql;
 
 import de.samply.share.essentialquery.EssentialSimpleQueryDto;
 
+import java.util.Collections;
+
 public class EssentialSimpleQueryDto2CqlTransformer {
+
+    private static final CqlConfig.Codesystem SAMPLE_MATERIAL_TYPE = new CqlConfig.Codesystem("SampleMaterialType",
+            "https://fhir.bbmri.de/CodeSystem/SampleMaterialType");
 
     private final CqlExpressionFactory cqlExpressionFactory;
 
@@ -11,15 +16,69 @@ public class EssentialSimpleQueryDto2CqlTransformer {
     }
 
     public String toQuery(EssentialSimpleQueryDto essentialSimpleQueryDto, String entityType) {
-        CqlCodesytemDefinitionsFactory codesytemDefinitionsFactory = new CqlCodesytemDefinitionsFactory(cqlExpressionFactory);
+        CqlCodeSystemDefinitionsFactory codeSystemDefinitionsFactory = new CqlCodeSystemDefinitionsFactory(cqlExpressionFactory);
         CqlSingletonStatementsFactory singletonsFactory = new CqlSingletonStatementsFactory(cqlExpressionFactory);
         CqlFieldExpressionFactory fieldExpressionFactory = new CqlFieldExpressionFactory(cqlExpressionFactory);
         CqlPredicateFactory predicateFactory = new CqlPredicateFactory(fieldExpressionFactory);
 
-        String codesystemDefinitions = codesytemDefinitionsFactory.create(essentialSimpleQueryDto);
+        String codeSystemDefinitions = codeSystemDefinitionsFactory.create(essentialSimpleQueryDto, SAMPLE_MATERIAL_TYPE);
         String singletonStatements = singletonsFactory.create(essentialSimpleQueryDto, entityType);
-        String cqlPredicate = predicateFactory.create(essentialSimpleQueryDto, entityType);
+        String predicate = predicateFactory.create(essentialSimpleQueryDto, entityType);
 
-        return cqlExpressionFactory.getPreamble(entityType, codesystemDefinitions, singletonStatements) + cqlPredicate;
+        return cqlExpressionFactory.createLibrary(entityType, codeSystemDefinitions, singletonStatements,
+                predicate, createStratifierStatements(entityType));
+    }
+
+    private String createStratifierStatements(String entityType) {
+        switch (entityType) {
+            case "Patient":
+                return "define Gender:\n" +
+                        "  Patient.gender\n" +
+                        "\n" +
+                        "define AgeClass:\n" +
+                        "  (AgeInYears() div 10) * 10";
+            case "Specimen":
+                return "define TypeCodes:\n" +
+                        "  from Specimen.type.coding C return FHIRHelpers.ToCode(C)\n" +
+                        "\n" +
+                        "define SampleMaterialTypeCategory:\n" +
+                        "  case\n" +
+                        "    when\n" +
+                        "      exists (TypeCodes intersect {\n" +
+                        "        Code 'whole-blood' from SampleMaterialType,\n" +
+                        "        Code 'bone-marrow' from SampleMaterialType,\n" +
+                        "        Code 'buffy-coat' from SampleMaterialType,\n" +
+                        "        Code 'peripheral-blood-cells-vital' from SampleMaterialType,\n" +
+                        "        Code 'blood-plasma' from SampleMaterialType,\n" +
+                        "        Code 'plasma-edta' from SampleMaterialType,\n" +
+                        "        Code 'plasma-citrat' from SampleMaterialType,\n" +
+                        "        Code 'plasma-heparin' from SampleMaterialType,\n" +
+                        "        Code 'plasma-cell-free' from SampleMaterialType,\n" +
+                        "        Code 'plasma-other' from SampleMaterialType,\n" +
+                        "        Code 'blood-serum' from SampleMaterialType,\n" +
+                        "        Code 'ascites' from SampleMaterialType,\n" +
+                        "        Code 'csf-liquor' from SampleMaterialType,\n" +
+                        "        Code 'urine' from SampleMaterialType,\n" +
+                        "        Code 'liquid-other' from SampleMaterialType\n" +
+                        "      })\n" +
+                        "    then 'liquid'\n" +
+                        "    when\n" +
+                        "      exists (TypeCodes intersect {\n" +
+                        "        Code 'tissue-ffpe' from SampleMaterialType,\n" +
+                        "        Code 'tumor-tissue-ffpe' from SampleMaterialType,\n" +
+                        "        Code 'normal-tissue-ffpe' from SampleMaterialType,\n" +
+                        "        Code 'other-tissue-ffpe' from SampleMaterialType,\n" +
+                        "        Code 'tissue-frozen' from SampleMaterialType,\n" +
+                        "        Code 'tumor-tissue-frozen' from SampleMaterialType,\n" +
+                        "        Code 'normal-tissue-frozen' from SampleMaterialType,\n" +
+                        "        Code 'other-tissue-frozen' from SampleMaterialType,\n" +
+                        "        Code 'tissue-other' from SampleMaterialType\n" +
+                        "      })\n" +
+                        "    then 'tissue'\n" +
+                        "    else 'other'\n" +
+                        "  end";
+            default:
+                return "";
+        }
     }
 }
---
+++ src/test/java/de/samply/share/broker/utils/cql/CqlCodeSystemDefinitionsFactoryTest.java
@@ -0,0 +1,207 @@
+package de.samply.share.broker.utils.cql;
+
+import com.google.common.collect.ImmutableSet;
+import de.samply.share.essentialquery.EssentialSimpleFieldDto;
+import de.samply.share.essentialquery.EssentialSimpleQueryDto;
+import de.samply.share.essentialquery.EssentialSimpleValueDto;
+import org.apache.commons.lang.StringUtils;
+import org.easymock.EasyMock;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static de.samply.share.broker.utils.cql.CqlTestHelper.*;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class CqlCodeSystemDefinitionsFactoryTest {
+
+    private static final String EXPECTED_CODESYSTEM_DEFINITION_1 = "codesystem " + CODESYSTEM_NAME_1 + ": '" + CODESYSTEM_URL_1 + "'";
+    private static final String EXPECTED_CODESYSTEM_DEFINITION_2 = "codesystem " + CODESYSTEM_NAME_2 + ": '" + CODESYSTEM_URL_2 + "'";
+
+    private CqlExpressionFactory expressionFactory;
+    private CqlCodeSystemDefinitionsFactory codeSystemDefinitionsFactory;
+
+    @BeforeEach
+    void initFactory() {
+        expressionFactory = EasyMock.createNiceMock(CqlExpressionFactory.class);
+
+        codeSystemDefinitionsFactory = new CqlCodeSystemDefinitionsFactory(expressionFactory);
+    }
+
+    @Test
+    void test_create_emptyDto() {
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(new EssentialSimpleQueryDto());
+        assertThat("Error creating code system definition for empty DTO.", CqlTestHelper.trim(definitions), is(""));
+    }
+
+    @Test
+    void test_create_emptyDto_oneFixCodeSystem() {
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(new EssentialSimpleQueryDto(),
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1));
+        assertThat(CqlTestHelper.trim(definitions), is(EXPECTED_CODESYSTEM_DEFINITION_1));
+    }
+
+    @Test
+    void test_create_WithoutCodeSystem() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1);
+
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(Collections.emptySet());
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto);
+        assertThat("Error creating code system definition for one field without code system.", CqlTestHelper.trim(definitions), is(""));
+    }
+
+    @Test
+    void test_create_WithCodeSystem() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1);
+
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1)));
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto);
+        assertThat("Error creating code system definition for one field with code system.", CqlTestHelper.trim(definitions), is(EXPECTED_CODESYSTEM_DEFINITION_1));
+    }
+
+    @Test
+    void test_create_WithCodeSystem_oneFixCodeSystem() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1);
+
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1)));
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto,
+                createCodeSystem(CODESYSTEM_NAME_2, CODESYSTEM_URL_2));
+        assertThat("Wrong number of code system definitions for SampleMaterialType.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_1), is(1));
+        assertThat("Wrong number of code system definitions for StorageTemperature.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_2), is(1));
+    }
+
+    @Test
+    void test_create_WithSameCodeSystemFromQueryDtoAndFixCodeSystem() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1);
+
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1)));
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto,
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1));
+        assertThat("Error creating code system definition for one field with code system.", CqlTestHelper.trim(definitions), is(EXPECTED_CODESYSTEM_DEFINITION_1));
+    }
+
+    @Test
+    void test_create_WithTwoDifferentCodeSystems() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1, MDR_URN_2);
+
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1)));
+
+        expect(expressionFactory.getCodesystems(MDR_URN_2)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_2, CODESYSTEM_URL_2)));
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto);
+        assertThat("Wrong number of code system definitions for SampleMaterialType.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_1), is(1));
+        assertThat("Wrong number of code system definitions for StorageTemperature.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_2), is(1));
+    }
+
+    @Test
+    void test_create_WithTwoDifferentCodeSystemsInOneField() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1);
+
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1),
+                createCodeSystem(CODESYSTEM_NAME_2, CODESYSTEM_URL_2)));
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto);
+        assertThat("Wrong number of code system definitions for SampleMaterialType.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_1), is(1));
+        assertThat("Wrong number of code system definitions for StorageTemperature.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_2), is(1));
+    }
+
+    @Test
+    void test_create_WithTwoDifferentCodeSystemsInTwoEntities() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1, MDR_URN_2);
+
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1)));
+
+        expect(expressionFactory.getCodesystems(MDR_URN_2)).andReturn(ImmutableSet.of(
+                createCodeSystem(CODESYSTEM_NAME_2, CODESYSTEM_URL_2)));
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto);
+        assertThat("Wrong number of code system definitions for SampleMaterialType.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_1), is(1));
+        assertThat("Wrong number of code system definitions for StorageTemperature.", StringUtils.countMatches(CqlTestHelper.trim(definitions), EXPECTED_CODESYSTEM_DEFINITION_2), is(1));
+    }
+
+    @Test
+    void test_create_WithTheSameCodeSystemInTwoEntities() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1, MDR_URN_2);
+
+        Set<CqlConfig.Codesystem> expectedCodeSystem =
+                ImmutableSet.of(createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1));
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(expectedCodeSystem);
+        expect(expressionFactory.getCodesystems(MDR_URN_2)).andReturn(expectedCodeSystem);
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto);
+        assertThat("Error creating code system definition for two fields in two entities with the same code system.", CqlTestHelper.trim(definitions), is(EXPECTED_CODESYSTEM_DEFINITION_1));
+    }
+
+    @Test
+    void test_create_WithEqualCodeSystemInTwoEntities() {
+        EssentialSimpleQueryDto queryDto = createDtoWithFields(MDR_URN_1, MDR_URN_2);
+
+        Set<CqlConfig.Codesystem> expectedCodeSystem1 =
+                ImmutableSet.of(createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1));
+        expect(expressionFactory.getCodesystems(MDR_URN_1)).andReturn(expectedCodeSystem1);
+        Set<CqlConfig.Codesystem> expectedCodeSystem2 =
+                ImmutableSet.of(createCodeSystem(CODESYSTEM_NAME_1, CODESYSTEM_URL_1));
+        expect(expressionFactory.getCodesystems(MDR_URN_2)).andReturn(expectedCodeSystem2);
+
+        replay(expressionFactory);
+
+        String definitions = codeSystemDefinitionsFactory.create(queryDto);
+        assertThat("Error creating code system definition for two fields in two entities with the same code system.", CqlTestHelper.trim(definitions), is(EXPECTED_CODESYSTEM_DEFINITION_1));
+    }
+
+    @NotNull
+    private EssentialSimpleQueryDto createDtoWithFields(String... urnList) {
+        List<EssentialSimpleFieldDto> fieldDtos = new ArrayList<>();
+
+        for (String mdrUrn : urnList) {
+            EssentialSimpleValueDto valueDto = CqlTestHelper.createValueDto(11.0, 47.0);
+            EssentialSimpleFieldDto fieldDto = createFieldDto(mdrUrn, valueDto);
+            fieldDtos.add(fieldDto);
+        }
+
+        return CqlTestHelper.createSimpleQueryDto(fieldDtos.toArray(new EssentialSimpleFieldDto[]{}));
+    }
+
+    private CqlConfig.Codesystem createCodeSystem(String name, String url) {
+        CqlConfig.Codesystem codeSystem = new CqlConfig.Codesystem();
+        codeSystem.setName(name);
+        codeSystem.setUrl(url);
+        return codeSystem;
+    }
+}
