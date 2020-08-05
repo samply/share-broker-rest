5a2ab2489c42afedf8820343de8cc384bc4fd9ab
Deniz Tas <d.tas@dkfz-heidelberg.de>
2020-08-05 14:09:09 +0200

    fixed chars in start.sh

--- src/docker/start.sh
+++ src/docker/start.sh
@@ -24,9 +24,9 @@ sed -i "s/{proxy-host}/${PROXY_HOST}/"                  /usr/local/tomcat/conf/s
 sed -i "s/{proxy-port}/${PROXY_PORT}/"                  /usr/local/tomcat/conf/samply_common_config.xml
 
 sed -i "s/{broker-name}/${BROKER_NAME}/"                /usr/local/tomcat/conf/samply.share.broker.conf
-sed -i "s/{mdr-url}/${MDR_URL}/"                        /usr/local/tomcat/conf/samply.share.broker.conf
-sed -i "s/{icinga-host}/${ICINGA_HOST}/"                /usr/local/tomcat/conf/samply.share.broker.conf
-sed -i "s/{icinga-path}/${ICINGA_PATH}/"                /usr/local/tomcat/conf/samply.share.broker.conf
+sed -i "s#{mdr-url}#${MDR_URL}#"                        /usr/local/tomcat/conf/samply.share.broker.conf
+sed -i "s#{icinga-host}#${ICINGA_HOST}#"                /usr/local/tomcat/conf/samply.share.broker.conf
+sed -i "s#{icinga-path}#${ICINGA_PATH}#"                /usr/local/tomcat/conf/samply.share.broker.conf
 sed -i "s/{icinga-username}/${ICINGA_USERNAME}/"        /usr/local/tomcat/conf/samply.share.broker.conf
 sed -i "s/{icinga-password}/${ICINGA_PASSWORD}/"        /usr/local/tomcat/conf/samply.share.broker.conf
 sed -i "s/{icinga-site-suffix}/${ICINGA_SITE_SUFFIX}/"  /usr/local/tomcat/conf/samply.share.broker.conf
